128ff56a384b655ee897d1871feae7c3b2bd6717
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2024-09-07 00:40:02 +0400

    green: not exact match prints more details

--- tests/test_similar_buffers.py
+++ tests/test_similar_buffers.py
@@ -3,15 +3,38 @@ from typing import Union
 
 
 class SimilarBufferDetector:
-    def statistics(self, a: bytearray, b: bytearray) -> dict[str, dict[str, Union[int, bytearray]]]:
+    def statistics(self, a: bytearray, b: bytearray) -> list[dict[str, dict[str, Union[int, bytearray]]]]:
         if a == b:
-            return {'exactlySameContents': {
+            return [{'exactlySameContents': {
                 'start_a': 0,
                 'start_b': 0,
                 'length': len(a),
                 'matched': a
-            }}
-        return {}
+            }}]
+
+        matches = []
+        current_match = []
+        for i in range(len(a)):
+            if a[i] == b[i]:
+                current_match.append(i)
+            else:
+                if current_match:
+                    matches.append(current_match)
+                    current_match = []
+
+        m = []
+        for i in range(len(matches)):
+            start_a = matches[i][0]
+            start_b = matches[i][0]
+            length = len(matches[i])
+            matched = a[start_a:start_a + length]
+            m.append({'sameContents': {
+                'start_a': start_a,
+                'start_b': start_b,
+                'length': length,
+                'matched': matched
+            }})
+        return m
 
 
 class SimilarBufferDetectorTest(unittest.TestCase):
@@ -21,17 +44,22 @@ class SimilarBufferDetectorTest(unittest.TestCase):
     def test_exact_match(self) -> None:
         actual = self.detector.statistics(bytearray(b"Hello, World!"), bytearray(b"Hello, World!"))
 
-        self.assertEqual({'exactlySameContents': {
+        self.assertEqual([{'exactlySameContents': {
             'start_a': 0,
             'start_b': 0,
             'length': 13,
             'matched': bytearray(b"Hello, World!")
-        }}, actual)
+        }}], actual)
 
     def test_not_exact_match(self) -> None:
         actual = self.detector.statistics(bytearray(b"Hello, John!"), bytearray(b"Hello, World!"))
 
-        self.assertEqual({}, actual)
+        self.assertEqual([
+            {'sameContents': {'start_a': 0, 'start_b': 0, 'length': len(bytearray(b"Hello, ")),
+                              'matched': bytearray(b"Hello, ")}},
+            {'sameContents': {'start_a': 8, 'start_b': 8, 'length': 1, 'matched': bytearray(b"o")}}
+        ],
+            actual)
 
 
 if __name__ == '__main__':
