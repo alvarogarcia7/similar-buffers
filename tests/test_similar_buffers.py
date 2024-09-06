22098c869c8f0aff77b20e23cab6abaeacb27e95
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2024-09-07 01:01:37 +0400

    green: copy unit test for SameContents

--- tests/test_similar_buffers.py
+++ tests/test_similar_buffers.py
@@ -85,5 +85,44 @@ class ExactlySameContentsTest(unittest.TestCase):
         self.assertEqual([], actual)
 
 
+class SameContentsTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.statistic = SameContents()
+
+    def test_exact_match(self) -> None:
+        actual = self.statistic.detect(
+            bytearray(b"Hello, World!"), bytearray(b"Hello, World!")
+        )
+
+        self.assertEqual([], actual)
+
+    def test_not_exact_match(self) -> None:
+        actual = self.statistic.detect(
+            bytearray(b"Hello, John!"), bytearray(b"Hello, World!")
+        )
+
+        self.assertEqual(
+            [
+                {
+                    "sameContents": {
+                        "start_a": 0,
+                        "start_b": 0,
+                        "length": len(bytearray(b"Hello, ")),
+                        "matched": bytearray(b"Hello, "),
+                    }
+                },
+                {
+                    "sameContents": {
+                        "start_a": 8,
+                        "start_b": 8,
+                        "length": 1,
+                        "matched": bytearray(b"o"),
+                    }
+                },
+            ],
+            actual,
+        )
+
+
 if __name__ == "__main__":
     unittest.main()
